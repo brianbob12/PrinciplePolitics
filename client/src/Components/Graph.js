e5edaeb7db9b1659eebc0bc0467e5f9faef6862b
Cyrus Singer <japaneserhino@gmail.com>
2021-11-04 10:25:03 +0000

    Adding graph component and making changes to home page

---
+++ client/src/Components/Graph.js
@@ -0,0 +1,15 @@
+
+
+export default ({ size, points }) => {
+  //canvas
+  return (
+    <div
+      style={{
+        height: size,
+        width: size,
+        backgroundColor: "white"
+      }}
+    >
+    </div>
+  )
+}
